5e7583f0053ffd0ed0d724bc6ef7e2862d6d70dd
Jay-Ar Brouillard <brouillardjr@gmail.com>
2019-11-02 20:16:45 -0500

    SQL updates

--- app/src/main/sql/MusclesTrained.sql
+++ app/src/main/sql/MusclesTrained.sql
@@ -1,7 +1,7 @@
 CREATE TABLE MusclesTrained (
 	muscles_trained_id int NOT NULL AUTO_INCREMENT,
 	exercise_id int NOT NULL,
-	muscle_group enum('quadriceps', 'hamstrings', 'calves', 'chest', 'back', 'shoulders', 'biceps', 'forearms'),
+	muscle_group enum('quadriceps', 'hamstrings', 'calves', 'chest', 'neck', 'trapezius', 'lats', 'erector spinae', 'shoulders', 'biceps', 'forearms', 'triceps', 'glutes'),
 	PRIMARY KEY (muscles_trained_id),
 	FOREIGN KEY (exercise_id) REFERENCES Exercises(exercise_id)
 	ON DELETE CASCADE
--- app/src/main/sql/WorkoutExercises.sql
+++ app/src/main/sql/WorkoutExercises.sql
@@ -1,12 +1,12 @@
 CREATE TABLE WorkoutExercises (
 	workout_exercise_id int NOT NULL AUTO_INCREMENT,
-	routine_workout_id int NOT NULL,
+	workout_id int NOT NULL,
 	exercise_id int NOT NULL,
 	minimum_sets int DEFAULT NULL,
 	minimum_reps int DEFAULT NULL,
 	maximum_sets int DEFAULT NULL,
 	maximum_reps int DEFAULT NULL,
 	PRIMARY KEY (workout_exercise_id),
-	FOREIGN KEY (routine_workout_id) REFERENCES RoutineWorkouts(routine_workout_id),
+	FOREIGN KEY (workout_id) REFERENCES Workouts(workout_id),
 	FOREIGN KEY (exercise_id) REFERENCES Exercises(exercise_id)
 );
